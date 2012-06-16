87378f41b782dd0342a4c156440fb8469f17bd05
Loy van Beek <loy.vanbeek@gmail.com>
2012-06-16 19:38:26 +0200

    Set the app to be full-screen. Images are now referenced to by activity, next is to load the paths for each image from configuration

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import Tkinter as tk
+import tkFont
 import yaml
 import pyQRjotari
 import CsvInterface
@@ -18,37 +19,41 @@ import CsvInterface
 
 class QrJotariGui(object):
 
-    def __init__(self, master):
+    def __init__(self, master, activity2img_map):
+        self.act2img = dict([(act, tk.PhotoImage(file=path, height=200)) for act, path in activity2img_map.iteritems()])
+
         frame = tk.Frame(master)
         frame.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+
+        self.customFont = tkFont.Font(family="Helvetica", size=50)
         
         self.dateframe = tk.Frame(frame)
         #Build the datecontrol frame
         self.dateframe.grid(row=1)#pack())
         
         self.groupText = tk.StringVar()
-        self.groupLabel = tk.Label(frame, textvariable=self.groupText)
+        self.groupLabel = tk.Label(frame, textvariable=self.groupText, font=self.customFont)
         self.groupLabel.grid(row=2)#pack()#tk.TOP
                 
-        self.goto_firstLabel = tk.Label(frame, text="gaat naar")
+        self.goto_firstLabel = tk.Label(frame, text="gaat naar", font=self.customFont)
         self.goto_firstLabel.grid(row=3)#pack(side=tk.LEFT)
         
         self.currentActivityFrame = tk.Frame(frame)
         
         self.activity_firstText = tk.StringVar()
-        self.activity_firstLabel = tk.Label(self.currentActivityFrame, textvariable=self.activity_firstText)
+        self.activity_firstLabel = tk.Label(self.currentActivityFrame, textvariable=self.activity_firstText, font=self.customFont)
         self.activity_firstLabel.pack(side=tk.LEFT)
-        self.activity_firstImage = tk.PhotoImage(file="/home/loy/Development/pyQrJotari/images/tenten opzetten.GIF")
+        self.activity_firstImage = tk.PhotoImage(file="/home/loy/Development/pyQrJotari/images/tenten opzetten.GIF", height=200)
         self.activity_firstImLbl = tk.Label(self.currentActivityFrame, image=self.activity_firstImage)
         self.activity_firstImLbl.pack(side=tk.RIGHT)
         
         self.currentActivityFrame.grid(row=4,sticky=tk.N+tk.S+tk.E+tk.W)#pack()
         
-        self.goto_secondLabel = tk.Label(frame, text="daarna")
+        self.goto_secondLabel = tk.Label(frame, text="daarna", font=self.customFont)
         self.goto_secondLabel.grid(row=5)#pack()
         
         self.activity_secondText = tk.StringVar()
-        self.activity_secondLabel = tk.Label(frame, textvariable=self.activity_secondText)
+        self.activity_secondLabel = tk.Label(frame, textvariable=self.activity_secondText, font=self.customFont)
         self.activity_secondLabel.grid(row=6)#pack()
         
         self.groupText.set("Groep 1")
@@ -56,8 +61,9 @@ class QrJotariGui(object):
         self.activity_secondText.set("Slapen")
     
     def update(self, age, group, group_activity, current_time, image):
-        self.groupText.set(group)
+        self.groupText.set("Groep "+str(group))
         self.activity_firstText.set(group_activity)
+        self.activity_firstImLbl.configure(image=self.act2img['jota'])
 
 def main(config):
     timeformat = [item['timeformat'] for item in config if item.has_key("timeformat")][0] #load timeformat markup
@@ -68,7 +74,10 @@ def main(config):
         print args
         
     root = tk.Tk()
-    app = QrJotariGui(root)
+    
+    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(),
+                                       root.winfo_screenheight()))
+    app = QrJotariGui(root, {"jota":"/home/loy/Development/pyQrJotari/images/jota.gif"})
     
     backend = pyQRjotari.JotariQrBackend(schedules, app.update)
     backend.start()
