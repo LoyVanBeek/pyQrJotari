9203048399f503e5622c0773d67954c668898d0a
Loy van Beek <loy.van.beek@sioux.eu>
2013-09-19 15:13:47 +0200

    Removed obsolete comments in qr_generator, started web interface using bottle

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(  name='pyQrJotari',
         url='https://github.com/yol/pyQrJotari',
         description='Read QR-codes and display the Jotari activity',
         long_description="Kids can use this to scan their personal QR-code and the program will show what activity is next for them",
-        install_requires=["PIL"]
+        install_requires=["PIL", "bottle"]
 )
---
+++ webinterface.py
@@ -0,0 +1,9 @@
+from bottle import route, run, template
+
+from pyQRjotari import 
+
+@route('/qr/<group>')
+def index(group='klein1'):
+    return template('<b>Je moet naar {{group}}</b>!', group=group)
+
+run(host='localhost', port=8080)
