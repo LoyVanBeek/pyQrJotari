f16aa5aca7890a51f5379ac30d28dc24d653230e
Loy van Beek <loy.vanbeek@gmail.com>
2013-09-30 23:01:27 +0200

    First version of webinterface. csv_interface is not yet complete, but server only needs to be changed to whatever format Tom likes

--- webinterface.py
+++ webinterface.py
@@ -1,10 +1,23 @@
 #! /usr/bin/python
 from bottle import route, run, template
+import datetime
+from dateutil import parser
+from csv_interface import build_interface
 
-#from pyQRjotari import 
+klein, groot = build_interface()
+schedules = {"klein":klein, "groot":groot}
 
-@route('/qr/<group>')
-def index(group='klein1'):
-    return template('<b>Je moet naar {{group}}</b>!', group=group)
+@route('/qr/<code>/<time>')
+def index(code='klein1', time=None):
+    age = code[:5]
+    group = int(code[5:])
+    if age in schedules:
+        if time:
+            time = parser.parse(time)
+        if not time: 
+            time = datetime.datetime.now()
+        activity = schedules[age][time][group]
+        return template('<b>Je moet naar {{activity}}</b>!', activity=activity)
 
-run(host='localhost', port=8080)
+if __name__ == "__main__":
+    run(host='localhost', port=8080)
