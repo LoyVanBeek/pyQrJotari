bbaeb86ee52ede1e182a69470607082da2ff729a
Loy van Beek <loy.vanbeek@gmail.com>
2013-10-03 23:07:11 +0200

    Enabled auto-reloading for the web interface

--- webinterface.py
+++ webinterface.py
@@ -22,4 +22,4 @@ def index(code='klein1', time=None):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
-    run(host='0.0.0.0', port=port, debug=True)
+    run(host='0.0.0.0', port=port, debug=True, reloader=True)
