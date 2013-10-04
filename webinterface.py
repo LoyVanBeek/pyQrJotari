ddd4c01c8158392fee7ea79bbd8c1aa8dc00c4fe
Loy van Beek <loy.vanbeek@gmail.com>
2013-10-04 08:43:00 +0200

    Added a bit of error handling and default parameters for time. When there is no time given, default to now and when there is no program for that, report the rrror

--- webinterface.py
+++ webinterface.py
@@ -8,7 +8,8 @@ from csv_interface import build_interface
 klein, groot = build_interface()
 schedules = {"klein":klein, "groot":groot}
 
-@route('/qr/<code>/<time>')
+@route('/qr/<code>')
+@route('/qr/<code>/<time>', defaults={'time': None})
 def index(code='klein1', time=None):
     age = code[:5]
     group = int(code[5:])
@@ -17,8 +18,11 @@ def index(code='klein1', time=None):
             time = parser.parse(time)
         if not time: 
             time = datetime.datetime.now()
-        activity = schedules[age][time][group]
-        return template('<b>Je moet naar {{activity}}</b>!', activity=activity)
+        try:
+            activity = schedules[age][time][group]
+            return template('Je moet naar <b>{{activity}}</b>!', activity=activity)
+        except KeyError:
+            return "Het is nog geen jotari. Vul een datum en tijd in in de URL en probeer het nog eens"
 
 @route('/qr/reload')
 def reload():
