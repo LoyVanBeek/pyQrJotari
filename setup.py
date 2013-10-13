7ff7470fa7a33f13253fb4058ffdd2c3dffdeb98
Loy van Beek <loy.vanbeek@gmail.com>
2013-10-13 16:36:29 +0200

    Trying both distutils and setuptools in setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-#from distutils.core import setup
-from setuptools import setup
+try:
+    from distutils.core import setup
+except ImportError:
+    try:
+        from setuptools import setup
+    except ImportError:
+        print "Use the binary installers..."
 
 setup(  name='pyQrJotari',
         version='1.1',
         author='Loy van Beek',
         author_email='loy.vanbeek@gmail.com',
-        scripts=['gui.py'],
+        scripts=['gui.py', 'csv_interface.py'],
         url='https://github.com/yol/pyQrJotari',
         description='Read QR-codes and display the Jotari activity',
         long_description="Kids can use this to scan their personal QR-code and the program will show what activity is next for them",
